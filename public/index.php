b028cf14357d19343b423d7e78bb51fba98bdd16
Séra Bálint <sera.balint@e-vista.hu>
2016-02-21 14:38:43 +0100

    updates for new additions

--- public/index.php
+++ public/index.php
@@ -24,25 +24,80 @@ $crawler = new Crawler();
 // Initialize form transpilation service (dependency injection friendly interface)
 $formService = new Service($crawler);
 
-$twig = new Twig_Environment($loader, array(
+$twig = new Twig_Environment(
+    $loader, array(
     'cache' => '../var/cache',
-));
+)
+);
 $twig->clearCacheFiles();
 
-$router->addRoute('GET', '/form', function (Request $request, Response $response) use($twig) {
-    // do something clever
-    $response = new Response($twig->render('form.twig.html', []));
-    return $response;
-});
+$router->addRoute(
+    'GET',
+    '/form',
+    function (Request $request, Response $response) use ($twig) {
+        // do something clever
+        $response = new Response($twig->render('form.twig.html', []));
 
-$router->addRoute('POST', '/loginform', function (Request $request, Response $response) use($twig, $crawler, $formService) {
+        return $response;
+    }
+);
 
-    $formMarkup = $request->request->get('serform');
-    $form = $formService->transpileForm($formMarkup);
+$router->addRoute(
+    'POST',
+    '/displayform',
+    function (Request $request, Response $response) use ($twig, $crawler, $formService) {
 
-    $response = new JsonResponse(['dump'=>(var_export($form, true))]);
-    return $response;
-});
+        $formMarkup = $request->request->get('serform');
+        $form = $formService->transpileForm($formMarkup);
+
+        // We just dump the object to enable
+        $response = new JsonResponse(['dump' => (var_export($form, true))]);
+
+        return $response;
+    }
+);
+
+// Use form data
+$router->addRoute(
+    'POST',
+    '/loginform',
+    function (Request $request, Response $response) use ($twig, $crawler, $formService) {
+
+        $formMarkup = $request->request->get('serform');
+        $form = $formService->transpileForm($formMarkup);
+
+        // Get fields:
+        $fields = $form->getFields();
+
+        // Use email
+        $emailField = $form->getField('email');
+
+        // Get attributes, eg. placeholder:
+        $placeholder = $emailField->getAttribute('placeholder');
+
+        // Get selected option:
+        $selectField = $form->getField('test-select');
+        $selected = $selectField->getValue();
+
+        // Get defaultly selected option (that was set selected in markup)
+        $defaultSelected = $selectField->getDefaultSelectedOption();
+
+        // Check validity
+        if (!$form->isValid()) {
+            // All errors can be spotted in the fields
+            foreach ($form->getFields() as $field) {
+                if (!$field->isValid()) {
+                    $validationErrors[] = $field->getErrors();
+                }
+            }
+        }
+
+        // We just dump the object to enable
+        $response = new JsonResponse(['dump' => (var_export($form, true))]);
+
+        return $response;
+    }
+);
 
 $dispatcher = $router->getDispatcher();
 $request = Request::createFromGlobals();
